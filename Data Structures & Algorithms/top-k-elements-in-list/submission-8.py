b185b5c2352fdfb1879c9b3e3ac944c54759ed10
Victor Grajski <victor.grajski@gmail.com>
2026-04-27 13:17:18 -0700

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,14 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        seen = defaultdict(int)
+        heap = []
+
+        for num in nums:
+            seen[num] += 1
+        for key in seen:
+            heapq.heappush(heap, (seen[key], key))
+        while len(heap) > k:
+            heapq.heappop(heap)
+        return [key for num, key in heap]
