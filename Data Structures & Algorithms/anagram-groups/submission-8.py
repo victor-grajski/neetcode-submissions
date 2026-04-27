3f3a969a221aaa168c62b365a6097a81dd71bd1e
Victor Grajski <victor.grajski@gmail.com>
2026-04-26 18:33:36 -0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = defaultdict(list)
+
+        for s in strs:
+            s_sorted = "".join(sorted(s))
+            seen[s_sorted].append(s)
+        return list(seen.values())
