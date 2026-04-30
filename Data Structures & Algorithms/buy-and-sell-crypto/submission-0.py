3025a487d2bfa7847f8a7855c18814a4ebe34216
Victor Grajski <victor.grajski@gmail.com>
2026-04-30 11:38:53 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+
+        l = 0
+        r = 1
+        profit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(profit, maxP)
+            else:
+                l = r
+            r += 1
+        return maxP
+        
