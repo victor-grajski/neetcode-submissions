340910d2a4366a72f8575100395afa9fb4985591
Victor Grajski <victor.grajski@gmail.com>
2026-05-11 19:04:23 -0700

    Add: two-integer-sum - submission-23

---
+++ Data Structures & Algorithms/two-integer-sum/submission-23.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            seen[num] = i
+        for i, num in enumerate(nums):
+            gap = target - num
+            if gap in seen and seen[gap] != i:
+                return [i, seen[gap]]
